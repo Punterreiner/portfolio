8c6be6149ad00e60a76da64e8ae5ff0ed4a8227d
Punterreiner <77292865+Punterreiner@users.noreply.github.com>
2023-01-04 16:40:45 -0600

    light/dark mode

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const toggleDark = document.querySelector('.dark-btn');
 
 navToggle.addEventListener('click', () =>{
     document.body.classList.toggle('nav-open');
@@ -9,4 +10,15 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
+})
+
+toggleDark.addEventListener('click', () => {
+    if (document.documentElement.getAttribute('data-theme') == 'dark'){ 
+        document.documentElement.setAttribute('data-theme', 'light');
+        toggleDark.textContent = "Dark Mode"
+    }
+    else{
+        document.documentElement.setAttribute('data-theme', 'dark');
+        toggleDark.textContent = "Light Mode"
+    }
 })
